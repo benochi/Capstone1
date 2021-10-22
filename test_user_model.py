aaf4046facf7f2e75584548be3423c75d20efbc8
Dan P <73758508+benochi@users.noreply.github.com>
2021-10-22 13:17:42 -0500

    Add files via upload

---
+++ test_user_model.py
@@ -0,0 +1,90 @@
+"""User model tests."""
+
+# run these tests like:
+#
+#    python -m unittest test_user_model.py
+
+
+import os
+from unittest import TestCase
+from sqlalchemy import exc
+
+from models import db, User
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///capstone-test"
+
+
+# Now we can import app
+
+from app import app
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+
+class UserModelTestCase(TestCase):
+    """Test user model signup"""
+
+    # Signup Tests
+   
+    def test_valid_register(self):
+        #username, pw, first_name, last_name, email
+        u_test = User.register("testtesttest","password", "testfirst", "testlast", "testtest@test.com")
+        uid = 99999
+        u_test.id = uid
+        db.session.commit()
+
+        u_test = User.query.get(uid)
+        self.assertIsNotNone(u_test)
+        self.assertEqual(u_test.username, "testtesttest")
+        self.assertEqual(u_test.email, "testtest@test.com")
+        self.assertNotEqual(u_test.password, "password")
+        # Bcrypt strings should start with $2b$
+        self.assertTrue(u_test.password.startswith("$2b$"))
+
+    def test_invalid_username_signup(self):
+        invalid = User.signup(None, "password",  None, None, "test@test.com")
+        uid = 123456789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_invalid_email_signup(self):
+        invalid = User.signup("testtest", "password", "bob", None, None)
+        uid = 123789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_invalid_password_signup(self):
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest",  None, "", None, "email@email.com")
+        
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@email.com", None, None, None)
+    
+    ####
+    #
+    # Authentication Tests
+    #
+    ####
+    def test_valid_authentication(self):
+        u = User.authenticate(self.u1.username, "password")
+        self.assertIsNotNone(u)
+        self.assertEqual(u.id, self.uid1)
+    
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("badusername", "password"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "badpassword"))
+
+
