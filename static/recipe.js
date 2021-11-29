4ff6d529113943035e0de4fd8550fc587ab55e9c
Dan P <73758508+benochi@users.noreply.github.com>
2021-11-29 07:20:47 -0600

    Update recipe.js

--- static/recipe.js
+++ static/recipe.js
@@ -59,7 +59,7 @@ function putStringErrorOnPage(val) {
     $recipeGrid.empty(); //clear existing recipes from the HTML grid
     // display error message with val
       const error = `<div class="alert alert-danger">
-                        <h2>ingredient 1: "${val}"" is not a valid ingredient</h2>
+                        <h2>ingredient 1: You must provide at least one ingredient</h2>
                       </div>`
       $recipeGrid.append(error);
     $recipeGrid.show();
